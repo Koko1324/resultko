199d4dee508b466eb015334057dbfb8897526ccb
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-13 23:02:33 +0900

    Add files via upload

--- result.py
+++ result.py
@@ -10,26 +10,21 @@ logo = Image.open("logo.png")
 #레이아웃 설정
 col1, col2, col3, col4 = st.columns([1,1,1,1])
 
-with col3,col4:
-    st.write('설명')#설명
-
 with col1:
     st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col2:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col3:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
+    st.write('설명')#설명
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
 with col4:
-    st.write("코스"+'abc')#abc에 'A','B','C'중 하나 저장
     st.write('a1234'+"일차")#a1234에 몇일차인지 입력
     st.write("장소")
-    st.image('장소이미지')
+    #st.image('장소이미지')
